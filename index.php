89d0d64d89356ef542ff3195e091241362ce2099
TigorImamHamdani <2031710058@student.polinema.ac.id>
2023-08-23 09:24:35 +0700

    latihan msib5

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Portfolio</title>
+
+    <!---Style css--->
+    <link rel="stylesheet" href="css/style.css" />
+    
+    <!---Icon Boxicon & Remixicon--->
+     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!---Google Font--->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <header>
+
+        <ul class="navlist">
+            <li><a herf="#">Home</a></li>
+            <li><a herf="#">About</a></li>
+            <li><a herf="#">Contact</a></li>
+        </ul>
+
+        <a herf="#" class="logo">TIGOR IMAM </a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </header>
+
+    <section class="desc">
+        <div class="desc-text">
+            <h5>Hello, My name is</h5>
+            <h1>Tigor Imam Hamdani</h1>
+            <h4>Saya dari Politeknik Negeri Malang</h4>
+            <h4>Jurusan Teknologi Informasi</h4>
+            <h4>Prodi Sistem Informasi Bisnis</h4>
+            <br>
+            <a href="https://www.instagram.com/tigorimam/">Contact Me <i class="ri-instagram-line"></i></a>
+            <a href="https://github.com/TigorImamHamdani">Github <i class="ri-github-line"></i></a>
+        </div>
+
+        <div class="desc-img">
+            <img src="images/main.png" width="300" heigth="300">
+        </div>
+    </section>
+</body>
+</html>
